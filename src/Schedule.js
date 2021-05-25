d970ae693f5c69c07e9d81003290709e7a81a695
Joshie-Finster <77668881+Joshie-Finster@users.noreply.github.com>
2021-05-25 11:20:00 +0100

    all?

---
+++ src/Schedule.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import "./App.scss";
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
+
+const Schedule = () => {
+  const [golData, setGolData] = useState([]);
+
+  useEffect(() => {
+    getGolData();
+  });
+  const getGolData = async () => {
+    console.log("hello");
+    const response = await fetch(
+      "https://www.golcentres.co.uk/results?LeagueId=393018.html"
+    )
+      .then((res) => res.text())
+      .then((body) => console.log(body));
+  };
+
+  return (
+    <div className="schedule">
+      <div className="table">
+        <h1> Division Table</h1>
+        <div></div>
+      </div>
+      <div className="fixtures">
+        <h1>fixtures list</h1>
+      </div>
+    </div>
+  );
+};
+export default Schedule;
