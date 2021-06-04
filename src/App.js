386087c56065d2ff1591482993a944f23a4f1b1b
Joshie-Finster <77668881+Joshie-Finster@users.noreply.github.com>
2021-06-04 14:22:24 +0100

    grids seem overly complex compared to flex. Too much typing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Schedule from "./Schedule";
 import "./App.scss";
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
--- src/Schedule.js
+++ src/Schedule.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.scss";
-import { Container, Table } from "semantic-ui-react";
+import { Container, Table, Grid, Segment } from "semantic-ui-react";
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 
@@ -22,67 +22,92 @@ const Schedule = () => {
 
   return (
     <Container className="schedule">
-      <Container className="divcontainer">
-        <h1> Division Table</h1>
+      <Grid stackable columns={2}>
+        <Grid.Column>
+          <Segment className="divcontainer">
+            <h1> Division Table</h1>
 
-        <Table celled className="divtable">
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Team</Table.HeaderCell>
-              <Table.HeaderCell>GP</Table.HeaderCell>
-              <Table.HeaderCell>W</Table.HeaderCell>
-              <Table.HeaderCell>L</Table.HeaderCell>
-              <Table.HeaderCell>D</Table.HeaderCell>
-              <Table.HeaderCell>GF</Table.HeaderCell>
-              <Table.HeaderCell>GA</Table.HeaderCell>
-              <Table.HeaderCell>GD</Table.HeaderCell>
-              <Table.HeaderCell>PTS</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {divTable.map((i) => (
-              <Table.Row>
-                <Table.Cell className="divTeam ">{i.Team}</Table.Cell>
-                <Table.Cell>{i.GP}</Table.Cell>
-                <Table.Cell>{i.W}</Table.Cell>
-                <Table.Cell>{i.L}</Table.Cell>
-                <Table.Cell>{i.D}</Table.Cell>
-                <Table.Cell>{i.GF}</Table.Cell>
-                <Table.Cell>{i.GA}</Table.Cell>
-                <Table.Cell>{i.GD}</Table.Cell>
-                <Table.Cell>{i.PTS}</Table.Cell>
+            <Table
+              collapsing
+              celled
+              unstackable
+              compact="very"
+              className="divtable"
+            >
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Team</Table.HeaderCell>
+                  <Table.HeaderCell>GP</Table.HeaderCell>
+                  <Table.HeaderCell>W</Table.HeaderCell>
+                  <Table.HeaderCell>L</Table.HeaderCell>
+                  <Table.HeaderCell>D</Table.HeaderCell>
+                  <Table.HeaderCell>GF</Table.HeaderCell>
+                  <Table.HeaderCell>GA</Table.HeaderCell>
+                  <Table.HeaderCell>GD</Table.HeaderCell>
+                  <Table.HeaderCell>PTS</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+
+              <Table.Body>
+                {divTable.map((i) => (
+                  <Table.Row>
+                    <Table.Cell className="divTeam ">{i.Team}</Table.Cell>
+                    <Table.Cell>{i.GP}</Table.Cell>
+                    <Table.Cell>{i.W}</Table.Cell>
+                    <Table.Cell>{i.L}</Table.Cell>
+                    <Table.Cell>{i.D}</Table.Cell>
+                    <Table.Cell>{i.GF}</Table.Cell>
+                    <Table.Cell>{i.GA}</Table.Cell>
+                    <Table.Cell>{i.GD}</Table.Cell>
+                    <Table.Cell>{i.PTS}</Table.Cell>
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </Table>
+          </Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment className="fixtures">
+            <h1>Fixture list</h1>
+            <Table
+              collapsing
+              striped
+              size="small"
+              compact="very"
+              unstackable
+              className="fixturelist"
+            >
+              <Table.Row className="titleRow">
+                <Table.HeaderCell>Home</Table.HeaderCell>
+                <Table.HeaderCell>Score</Table.HeaderCell>
+                <Table.HeaderCell>Away</Table.HeaderCell>
+                <Table.HeaderCell>Date</Table.HeaderCell>
+                <Table.HeaderCell>Time/Status</Table.HeaderCell>
               </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-      </Container>
-      <Container className="fixtures">
-        <h1>Fixture list</h1>
-        <tbody className="fixturelist">
-          <tr className="titleRow">
-            <th>Home</th>
-            <th>Score</th>
-            <th>Away</th>
-            <th>Date</th>
-            <th>Time/Status</th>
-          </tr>
-          {fixList
-            .filter(
-              (i) =>
-                (i.Home == "Well Done, He's 13") |
-                (i.Away === "Well Done, He's 13")
-            )
-            .map((i) => (
-              <tr className="row">
-                <td className="fixHome">{i.Home}</td>
-                <td className="fixScore">{i.Score}</td>
-                <td className="fixAway">{i.Away}</td>
-                <td className="fixDate">{i.Date}</td>
-                <td className="fixTS">{i["Time/Status"]}</td>
-              </tr>
-            ))}
-        </tbody>
-      </Container>
+
+              <Table.Body>
+                {fixList
+                  .filter(
+                    (i) =>
+                      (i.Home == "Well Done, He's 13") |
+                      (i.Away === "Well Done, He's 13")
+                  )
+                  .map((i) => (
+                    <Table.Row className="row">
+                      <Table.Cell className="fixHome">{i.Home}</Table.Cell>
+                      <Table.Cell className="fixScore">{i.Score}</Table.Cell>
+                      <Table.Cell className="fixAway">{i.Away}</Table.Cell>
+                      <Table.Cell className="fixDate">{i.Date}</Table.Cell>
+                      <Table.Cell className="fixTS">
+                        {i["Time/Status"]}
+                      </Table.Cell>
+                    </Table.Row>
+                  ))}
+              </Table.Body>
+            </Table>
+          </Segment>
+        </Grid.Column>
+      </Grid>
     </Container>
   );
 };
