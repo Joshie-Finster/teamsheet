ca56117d2a36abe7b4ba6513ad89734dab2fbd78
Joshie-Finster <77668881+Joshie-Finster@users.noreply.github.com>
2021-06-08 11:12:50 +0100

    accordion update

--- src/Schedule.js
+++ src/Schedule.js
@@ -14,14 +14,14 @@ const Schedule = (props) => {
     const newIndex = currentIndex === index ? -1 : index;
 
     setActiveIndex(newIndex);
-    console.log(activeIndex)
+    console.log(activeIndex);
   };
 
   return (
     <Container className="schedule">
       <Grid stackable columns={2}>
         <Grid.Column>
-          <Accordion >
+          <Accordion>
             <Segment raised className="divcontainer">
               <Accordion.Title
                 active={activeIndex === 0}
@@ -74,45 +74,57 @@ const Schedule = (props) => {
           </Accordion>
         </Grid.Column>
         <Grid.Column>
-          <Segment raised className="fixtures">
-            <h1>Fixture list</h1>
-            <Table
-              basic
-              collapsing
-              striped
-              compact="very"
-              unstackable
-              className="fixturelist"
-            >
-              <Table.Row className="titleRow">
-                <Table.HeaderCell>Home</Table.HeaderCell>
-                <Table.HeaderCell>Score</Table.HeaderCell>
-                <Table.HeaderCell>Away</Table.HeaderCell>
-                <Table.HeaderCell>Date</Table.HeaderCell>
-                <Table.HeaderCell>Time/Status</Table.HeaderCell>
-              </Table.Row>
+          <Accordion>
+            <Segment raised className="fixtures">
+              <Accordion.Title
+                active={activeIndex === 1}
+                index={1}
+                onClick={handleClick}
+              >
+                <h1>Fixture list</h1>
+              </Accordion.Title>
+              <Accordion.Content>
+                <Table
+                  basic
+                  collapsing
+                  striped
+                  compact="very"
+                  unstackable
+                  className="fixturelist"
+                >
+                  <Table.Row className="titleRow">
+                    <Table.HeaderCell>Home</Table.HeaderCell>
+                    <Table.HeaderCell>Score</Table.HeaderCell>
+                    <Table.HeaderCell>Away</Table.HeaderCell>
+                    <Table.HeaderCell>Date</Table.HeaderCell>
+                    <Table.HeaderCell>Time/Status</Table.HeaderCell>
+                  </Table.Row>
 
-              <Table.Body>
-                {fixList
-                  .filter(
-                    (i) =>
-                      (i.Home === "Well Done, He's 13") |
-                      (i.Away === "Well Done, He's 13")
-                  )
-                  .map((i) => (
-                    <Table.Row className="row">
-                      <Table.Cell className="fixHome">{i.Home}</Table.Cell>
-                      <Table.Cell className="fixScore">{i.Score}</Table.Cell>
-                      <Table.Cell className="fixAway">{i.Away}</Table.Cell>
-                      <Table.Cell className="fixDate">{i.Date}</Table.Cell>
-                      <Table.Cell className="fixTS">
-                        {i["Time/Status"]}
-                      </Table.Cell>
-                    </Table.Row>
-                  ))}
-              </Table.Body>
-            </Table>
-          </Segment>
+                  <Table.Body>
+                    {fixList
+                      .filter(
+                        (i) =>
+                          (i.Home === "Well Done, He's 13") |
+                          (i.Away === "Well Done, He's 13")
+                      )
+                      .map((i) => (
+                        <Table.Row className="row">
+                          <Table.Cell className="fixHome">{i.Home}</Table.Cell>
+                          <Table.Cell className="fixScore">
+                            {i.Score}
+                          </Table.Cell>
+                          <Table.Cell className="fixAway">{i.Away}</Table.Cell>
+                          <Table.Cell className="fixDate">{i.Date}</Table.Cell>
+                          <Table.Cell className="fixTS">
+                            {i["Time/Status"]}
+                          </Table.Cell>
+                        </Table.Row>
+                      ))}
+                  </Table.Body>
+                </Table>
+              </Accordion.Content>
+            </Segment>
+          </Accordion>
         </Grid.Column>
       </Grid>
     </Container>
