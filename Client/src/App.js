02498ec69275b6a4d74c0917dc1ff27b4a311973
Joshie-Finster <77668881+Joshie-Finster@users.noreply.github.com>
2021-06-16 09:47:17 +0100

    client/api split

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import Schedule from "./Schedule";
-
 import "./App.scss";
 import "semantic-ui-css/semantic.min.css";
 const fetch = require("node-fetch");
@@ -8,11 +7,24 @@ const fetch = require("node-fetch");
 function App() {
   const [divTable, setDivTable] = useState([]);
   const [fixList, setFixList] = useState([]);
+  const [APITest, setAPITest] = useState("");
 
   useEffect(() => {
+    callAPI();
     getGolData();
   }, []);
 
+  useEffect(() => {
+    
+  }, []);
+
+  const callAPI =async () => {
+    console.log('fetching API')
+    await fetch("http://localhost:9000/testAPI")
+      .then((res) => res.text())
+      .then((res) => setAPITest(res));
+      
+  };
   const getGolData = async () => {
     console.log("retrieving data from Gol");
     const response = await fetch(
@@ -29,6 +41,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="title">Teamsheet</h1>
+        <p className='app-intro'>{APITest}</p>
       </header>
       <Schedule divTable={divTable} fixList={fixList} />
       {/*<Squadsheet/>*/}
